5806c858fa56842a50bb70ea23a2fcc27e271365
hectorcapoblanco <cancer18ar@hotmail.com>
2022-06-26 21:32:36 -0300

    practica de Css

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app =express ();
+
+app.use(express.static("public"));
+
+app.listen (3000,()=>{console.log("El servidor esta corriendo")});
+
+app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))})
